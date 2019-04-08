b0892019f5e9a5bfd5807046e786afc3a6a1ac0e
k-nasa <htilcs1115@gmail.com>
2019-04-08 21:14:18 +0900

    Add impl grahiql

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate juniper;
+
+mod schema;
+
+use crate::schema::{create_schema, Schema};
+use actix_web::{middleware::Logger, web, App, Error, HttpResponse, HttpServer};
+use futures::Future;
+use juniper::http::graphiql::graphiql_source;
+use juniper::http::GraphQLRequest;
+use std::sync::Arc;
+
+fn graphiql() -> HttpResponse {
+    let html = graphiql_source("http://127.0.0.1:8080/graphql");
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(html)
+}
+
+fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web");
+    env_logger::init();
+
+    log::info!("server lunch!, localhost::8080");
+
+    let schema = std::sync::Arc::new(create_schema());
+
+    HttpServer::new(move || {
+        App::new()
+            .data(schema.clone())
+            .wrap(Logger::default())
+            .service(web::resource("/graphiql").route(web::get().to(graphiql)))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
 }
