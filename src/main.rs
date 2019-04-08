1da7c7e896d16ee57bf70f3a1e1c55df4f258ce8
k-nasa <htilcs1115@gmail.com>
2019-04-08 21:14:35 +0900

    add graphql endpoint

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,22 @@ fn graphiql() -> HttpResponse {
         .body(html)
 }
 
+fn graphql(
+    schema: web::Data<Arc<Schema>>,
+    request: web::Json<GraphQLRequest>,
+) -> impl Future<Item = HttpResponse, Error = Error> {
+    web::block(move || {
+        let res = request.execute(&schema, &());
+        Ok::<_, serde_json::error::Error>(serde_json::to_string(&res)?)
+    })
+    .map_err(Error::from)
+    .and_then(|res| {
+        Ok(HttpResponse::Ok()
+            .content_type("application/json")
+            .body(res))
+    })
+}
+
 fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web");
     env_logger::init();
@@ -29,6 +45,7 @@ fn main() -> std::io::Result<()> {
         App::new()
             .data(schema.clone())
             .wrap(Logger::default())
+            .service(web::resource("/graphql").route(web::post().to_async(graphql)))
             .service(web::resource("/graphiql").route(web::get().to(graphiql)))
     })
     .bind("127.0.0.1:8080")?
